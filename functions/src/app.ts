7d4360fd3f556d2acaa325bc5f3efd5ed9cc6242
Manuel Ospina <manuel@anect.jp>
2020-02-07 15:44:49 +0900

    Rename import

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,14 +1,12 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import { router } from './router';
-import { router as userRouter} from './users/router';
-import { router as programRouter} from './programs/router';
+import { router as programs} from './programs/router';
 
 const app = express();
 app.use(cors({origin: '*'}));
 
 app.use('/', router);
-app.use('/users', userRouter);
-app.use('/programs', programRouter)
+app.use('/users/:userId/programs', programs)
 
 export { app };
