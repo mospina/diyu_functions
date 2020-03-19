626001d00d257759327b7d3403a0005d1e9a3e3e
Manuel Ospina <manuel@anect.jp>
2020-03-19 15:39:30 +0900

    Add users

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,12 +1,14 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import { router } from './router';
+import { router as users} from './users/router';
 import { router as programs} from './programs/router';
 
 const app = express();
 app.use(cors({origin: '*'}));
 
 app.use('/', router);
+app.use('/users/', users)
 app.use('/users/:userId/programs', programs)
 
 export { app };
